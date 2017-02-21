6dae91b195b11c2e2ffffacab212e0db8927949d
Andrew Nguyen <andrewngu@gmail.com>
2017-02-21 10:12:17 -0800

    adding tag coloring

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -6,8 +6,20 @@ const propTypes = {
 };
 
 class Event extends Component {
+  tagClassName() {
+    const { severity } = this.props.event;
+
+    if (severity > 6) {
+      return 'tag--red';
+    } else if (severity > 3) {
+      return 'tag--orange';
+    }
+
+    return 'tag--green';
+  }
+
   render() {
-    const { description, name, timestamp } = this.props.event;
+    const { description, name, severity, timestamp } = this.props.event;
 
     return (
       <div className="event">
@@ -15,12 +27,17 @@ class Event extends Component {
           <div className="event__name">
             {name}
           </div>
+          <div className="event__severity">
+            <div className={`tag ${this.tagClassName()}`}>
+              {`Severity ${severity}`}
+            </div>
+          </div>
         </div>
         <div className="event__description">
           {description}
         </div>
         <div className="event__time">
-          {moment(timestamp, 'X').fromNow()}
+          {`Created ${moment(timestamp, 'X').fromNow()}`}
         </div>
       </div>
     );
